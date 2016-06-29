66b9139267615ce24acb07d83083ee5d13d83a7e
esproff <esproff@gmail.com>
2016-06-28 21:59:54 -0700

    Update TopicModelsVB.jl

--- src/TopicModelsVB.jl
+++ src/TopicModelsVB.jl
@@ -1,1 +1,21 @@
+module TopicModelsVB
+
+export VectorList, MatrixList, Document, Corpus, TopicModel, LDA, fLDA, CTM, fCTM, DTM, CTPF
+export isnegative, ispositive, tetragamma, partition
+export checkdoc, checkcorp, readcorp, writecorp, abridgecorp!, trimcorp!, compactcorp!, padcorp!, cullcorp!, fixcorp!, showdocs, getlex, getusers
+export train!
+export checkmodel, gendoc, gencorp, showtopics, showlibs, showdrecs, showurecs
+
+include("utils.jl")
+include("Corpus.jl")
+include("TopicModel.jl")
+include("LDA.jl")
+include("fLDA.jl")
+include("CTM.jl")
+include("fCTM.jl")
+include("DTM.jl")
+include("CTPF.jl")
+include("modelutils.jl")
+
+end
 
