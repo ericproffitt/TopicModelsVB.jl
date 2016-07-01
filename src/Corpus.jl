1ddb8cce84ec535a46687aad383dc761057497e9
esproff <esproff@gmail.com>
2016-07-01 03:42:55 -0700

    Update Corpus.jl

--- src/Corpus.jl
+++ src/Corpus.jl
@@ -371,23 +371,23 @@ getusers(corp::Corpus) = sort(collect(values(corp.users)))
 
 function readcorp(corpsym::Symbol)
 	if corpsym == :nsf
-		docfile = "~/.julia/v0.4/topicmodelsvb/datasets/nsf/nsfdocs.txt"
-		lexfile = "~/.julia/v0.4/topicmodelsvb/datasets/nsf/nsflex.txt"
-		titlefile = "~/.julia/v0.4/topicmodelsvb/datasets/nsf/nsftitles.txt"
+		docfile = pwd() * "/.julia/v0.4/topicmodelsvb/datasets/nsf/nsfdocs.txt"
+		lexfile = pwd() * "/.julia/v0.4/topicmodelsvb/datasets/nsf/nsflex.txt"
+		titlefile = pwd() * "/.julia/v0.4/topicmodelsvb/datasets/nsf/nsftitles.txt"
 		corp = readcorp(docfile=docfile, lexfile=lexfile, titlefile=titlefile, counts=true, stamps=true)
 
 	elseif corpsym == :citeu
-		docfile = "~/.julia/v0.4/topicmodelsvb/datasets/citeu/citeudocs.txt"
-		lexfile = "~/.julia/v0.4/topicmodelsvb/datasets/citeu/citeulex.txt"
-		userfile = "~/.julia/v0.4/topicmodelsvb/datasets/citeu/citeuusers.txt"
-		titlefile = "~/.julia/v0.4/topicmodelsvb/datasets/citeu/citeutitles.txt"
+		docfile = pwd() * "/.julia/v0.4/topicmodelsvb/datasets/citeu/citeudocs.txt"
+		lexfile = pwd() * "/.julia/v0.4/topicmodelsvb/datasets/citeu/citeulex.txt"
+		userfile = pwd() * "/.julia/v0.4/topicmodelsvb/datasets/citeu/citeuusers.txt"
+		titlefile = pwd() * "/.julia/v0.4/topicmodelsvb/datasets/citeu/citeutitles.txt"
 		corp = readcorp(docfile=docfile, lexfile=lexfile, userfile=userfile, titlefile=titlefile, counts=true, readers=true)
 		padcorp!(corp)
 
 	elseif corpsym == :mac
-		docfile = "~/.julia/v0.4/topicmodelsvb/datasets/mac/macdocs.txt"
-		lexfile = "~/.julia/v0.4/topicmodelsvb/datasets/mac/maclex.txt"
-		titlefile = "~/.julia/v0.4/topicmodelsvb/datasets/mac/mactitles.txt"
+		docfile = pwd() * "/.julia/v0.4/topicmodelsvb/datasets/mac/macdocs.txt"
+		lexfile = pwd() * "/.julia/v0.4/topicmodelsvb/datasets/mac/maclex.txt"
+		titlefile = pwd() * "/.julia/v0.4/topicmodelsvb/datasets/mac/mactitles.txt"
 		corp = readcorp(docfile=docfile, lexfile=lexfile, titlefile=titlefile, counts=true, stamps=true)
 
 	else
