ef5a40cf41645ef773010a4ed5ae3c34c19c56bf
Eric Proffitt <eric.ai@icloud.com>
2019-12-19 21:35:43 -0500

    Update gpuLDA.jl

--- src/gpuLDA.jl
+++ src/gpuLDA.jl
@@ -1,34 +1,16 @@
-mutable struct gpuLDA <: GPUTopicModel
+mutable struct gpuLDA <: TopicModel
 	K::Int
 	M::Int
 	V::Int
 	N::Vector{Int}
 	C::Vector{Int}
 	corp::Corpus
-	topics::VectorList{Int}
-	alpha::Vector{Float64}
-	beta::Matrix{Float64}
-	beta_old::Matrix{Float64}
-	beta_temp::Matrix{Float64}
-	Elogtheta::VectorList{Float64}
-	Elogtheta_old::VectorList{Float64}
-	gamma::VectorList{Float64}
-	phi::Matrix{Float64}
-
-	K::Int
-	M::Int
-	V::Int
-	N::Vector{Int}
-	C::Vector{Int}
-	corp::Corpus
-	batches::Vector{UnitRange{Int}}
 	topics::VectorList{Int}
 	alpha::Vector{Float32}
 	beta::Matrix{Float32}
+	Elogtheta::VectorList{Float32}
 	gamma::VectorList{Float32}
 	phi::MatrixList{Float32}
-	Elogtheta::VectorList{Float32}
-	Elogthetasum::Vector{Float32}
 	newbeta::Void
 	Npsums::VectorList{Int}
 	Jpsums::VectorList{Int}
@@ -59,7 +41,6 @@ mutable struct gpuLDA <: GPUTopicModel
 	Elogthetabuf::cl.Buffer{Float32}
 	Elogthetasumbuf::cl.Buffer{Float32}
 	elbo::Float32
-	newelbo::Float32
 
 	function gpuLDA(corp::Corpus, K::Integer, batchsize::Integer=length(corp))
 		K > 0 || throw(ArgumentError("Number of topics must be a positive integer."))
@@ -77,7 +58,7 @@ mutable struct gpuLDA <: GPUTopicModel
 		Elogtheta = [-Base.MathConstants.eulergamma * ones(K) .- digamma(K) for _ in 1:M]
 		Elogtheta_old = copy(Elogtheta)
 		gamma = [ones(K) for _ in 1:M]
-		phi = ones(K, N[1]) / K
+		phi = [ones(K, N[d]) / K for d in 1:M]
 		elbo = 0
 
 		model = new(K, M, V, N, C, copy(corp), topics, alpha, beta, beta_old, beta_temp, Elogtheta, Elogtheta_old, gamma, phi, elbo)
@@ -123,74 +104,87 @@ mutable struct gpuLDA <: GPUTopicModel
 	end
 end
 
-function Elogptheta(model::gpuLDA)
-	x = lgamma(sum(model.alpha)) - sum(lgamma.(model.alpha)) + dot(model.alpha - 1, model.Elogtheta)
+function Elogptheta(model::gpuLDA, d::Int)
+	"Compute E[log(P(theta))]."
+
+	x = loggamma(sum(model.alpha)) - sum(loggamma.(model.alpha)) + dot(model.alpha .- 1, model.Elogtheta[d])
 	return x
 end
 
 function Elogpz(model::gpuLDA, d::Int)
+	"Compute E[log(P(z))]."
+
 	counts = model.corp[d].counts
-	x = dot(model.phi[m] * counts, model.Elogtheta)
+	x = dot(model.phi * counts, model.Elogtheta)
 	return x
 end
 
 function Elogpw(model::gpuLDA, d::Int)
+	"Compute E[log(P(w))]."
+
 	terms, counts = model.corp[d].terms, model.corp[d].counts
 	x = sum(model.phi .* log.(@boink model.beta[:,terms]) * counts)
 	return x
 end
 
 function Elogqtheta(model::gpuLDA, d::Int)
+	"Compute E[log(q(theta))]."
+
 	x = -entropy(Dirichlet(model.gamma[d]))
 	return x
 end
 
 function Elogqz(model::gpuLDA, d::Int)
+	"Compute E[log(q(z))]."
+
 	counts = model.corp[d].counts
 	x = -sum([c * entropy(Categorical(model.phi[:,n])) for (n, c) in enumerate(counts)])
 	return x
 end
 
 function update_elbo!(model::gpuLDA)
+	"Update the evidence lower bound."
+
 	for d in 1:model.M
-		model.newelbo += Elogptheta(model, d, m) + Elogpz(model, d, m) + Elogpw(model, d, m) - Elogqtheta(model, d) - Elogqz(model, d, m)
+		model.elbo += Elogptheta(model, d) + Elogpz(model, d) + Elogpw(model, d) - Elogqtheta(model, d) - Elogqz(model, d)
 	end
 end
 
 function updateAlpha!(model::gpuLDA, niter::Integer, ntol::Real)
-	"Interior-point Newton method with log-barrier and back-tracking line search."
+	"Update alpha."
+	"Interior-point Newton's method with log-barrier and back-tracking line search."
 
-	@host model.alphabuf
-	@host model.Elogthetasumbuf
+	@host model.alpha_buffer
+	@host model.Elogthetasum_buffer
+
+	Elogtheta_sum = sum([model.Elogtheta[d] for d in 1:model.M])
 
 	nu = model.K
 	for _ in 1:niter
 		rho = 1.0
-		alphaGrad = Float32[nu / model.alpha[i] + model.M * (digamma(sum(model.alpha)) - digamma(model.alpha[i])) for i in 1:model.K] + model.Elogthetasum
-		alphaHessDiag = -(model.M * trigamma.(model.alpha) + (nu ./ model.alpha.^2))
-		p = (alphaGrad - sum(alphaGrad ./ alphaHessDiag) / (1 / (model.M * trigamma(sum(model.alpha))) + sum(1 ./ alphaHessDiag))) ./ alphaHessDiag
-
+		alpha_grad = [nu / model.alpha[i] + model.M * (digamma(sum(model.alpha)) - digamma(model.alpha[i])) for i in 1:model.K] .+ Elogtheta_sum
+		alpha_invhess_diag = -1 ./ (model.M * trigamma.(model.alpha) + nu ./ model.alpha.^2)
+		p = (alpha_grad .- dot(alpha_grad, alpha_invhess_diag) / (1 / (model.M * trigamma(sum(model.alpha))) + sum(alpha_invhess_diag))) .* alpha_invhess_diag
+		
 		while minimum(model.alpha - rho * p) < 0
 			rho *= 0.5
 		end	
 		model.alpha -= rho * p
 		
-		if (norm(alphaGrad) < ntol) & ((nu / model.K) < ntol)
+		if (norm(alpha_grad) < ntol) & (nu / model.K < ntol)
 			break
 		end
 		nu *= 0.5
 	end
 	@bumper model.alpha
 
-	model.Elogthetasum = zeros(model.K)
 	@buffer model.alpha
-	@buffer model.Elogthetasum
 end
 
 const LDA_BETA_c =
 """
 kernel void
-updateBeta(long K,
+update_beta(long K,
 			global float *newbeta,
 			global float *beta)
 	
@@ -206,7 +200,7 @@ updateBeta(long K,
 const LDA_BETA_NORM_c =
 """
 kernel void
-normalizeBeta(long K,
+normalize_beta(	long K,
 				long V,
 				global float *beta)
 
@@ -224,6 +218,9 @@ normalizeBeta(long K,
 				"""
 
 function updateBeta!(model::gpuLDA)
+	"Update beta"
+	"Analytic."
+
 	model.queue(model.betakern, (model.K, model.V), nothing, model.K, model.newbetabuf, model.betabuf)
 	model.queue(model.betanormkern, model.K, nothing, model.K, model.V, model.betabuf)
 end
@@ -231,7 +228,7 @@ end
 const LDA_NEWBETA_c =
 """
 kernel void
-updateNewbeta(long K,
+updateNewbeta(	long K,
 				const global long *Jpsums,
 				const global long *counts,
 				const global long *words,
@@ -255,38 +252,79 @@ function updateNewBeta!(model::gpuLDA)
 	model.queue(model.newbetakern, (model.K, model.V), nothing, model.K, model.Jpsumsbuf, model.countsbuf, model.wordsbuf, model.phibuf, model.newbetabuf)
 end
 
+const LDA_ELOGTHETA_c =
+"""
+$(DIGAMMA_c)
+
+kernel void
+update_Elogtheta(	long F,
+					long K,
+					const global float *gamma,
+					global float *Elogtheta)
+
+					{
+					long d = get_global_id(0);
+
+					float acc = 0.0f;
+
+					for (long i=0; i<K; i++)
+						acc += gamma[K * (F + d) + i];
+
+					acc = digamma(acc);	
+
+					for (long i=0; i<K; i++)
+						Elogtheta[K * d + i] = digamma(gamma[K * (F + d) + i]) - acc;
+					}
+					"""
+
+function updateElogtheta!(model::gpuLDA)
+	"Update E[log(theta)]."
+	"Analytic."
+	
+	model.queue(model.Elogtheta_kernel, model.M, nothing, 0, model.K, model.gamma_buffer, model.Elogtheta_buffer)
+end
+
 const LDA_GAMMA_cpp =
 """
 kernel void
-updateGamma(long F,
-			long K,
-			const global long *Npsums,
-			const global long *counts,
-			const global float *alpha,
-			const global float *phi,
-			global float *gamma)
+update_gamma(	long F,
+				long K,
+				const global long *Npsums,
+				const global long *counts,
+				const global float *alpha,
+				const global float *phi,
+				global float *gamma)
 
-			{   
-			long i = get_global_id(0);
-			long d = get_global_id(1);
+				{   
+				long i = get_global_id(0);
+				long d = get_global_id(1);
 
-			float acc = 0.0f;
+				float acc = 0.0f;
 
-			for (long n=Npsums[d]; n<Npsums[d+1]; n++)
-				acc += phi[K * n + i] * counts[n]; 
+				for (long n=Npsums[d]; n<Npsums[d+1]; n++)
+					acc += phi[K * n + i] * counts[n]; 
 
-			gamma[K * (F + d) + i] = alpha[i] + acc + $(EPSILON32);
-			}
-			"""
+				gamma[K * (F + d) + i] = alpha[i] + acc + $(EPSILON32);
+				}
+				"""
 
 function updateGamma!(model::gpuLDA)
+	"Update gamma."
+	"Analytic."
+
 	model.queue(model.gammakern, (model.K, model.M), nothing, 0, model.K, model.Npsumsbuf, model.countsbuf, model.alphabuf, model.phibuf, model.gammabuf)
 end
 
 const LDA_PHI_c =
 """
 kernel void
-update_phi(long K, const global long *Npsums, const global long *terms, const global float *beta, const global float *Elogtheta, global float *phi)
+update_phi(	long K,
+			const global,
+			long *Npsums,
+			const global long *terms,
+			const global float *beta,
+			const global float *Elogtheta,
+			global float *phi)
                                         
 			{
 			long i = get_global_id(0);
@@ -300,7 +338,8 @@ update_phi(long K, const global long *Npsums, const global long *terms, const gl
 const LDA_PHI_NORM_c =
 """
 kernel void
-normalize_phi(long K, global float *phi)
+normalize_phi(	long K,
+				global float *phi)
 				
 				{
 				long dn = get_global_id(0);
@@ -315,36 +354,12 @@ normalize_phi(long K, global float *phi)
 				}
 				"""
 
-function updatePhi!(model::gpuLDA)
+function update_phi!(model::gpuLDA)
+	"Update phi."
+	"Analytic."
+
 	model.queue(model.phi_kernel, (model.K, model.M), nothing, model.K, model.terms_buffer, model.beta_buffer, model.Elogtheta_buffer, model.phi_buffer)	
-	model.queue(model.phi_norm_kernel, sum(model.N[batch]), nothing, model.K, model.phi_buffer)
-end
-
-const LDA_ELOGTHETA_c =
-"""
-$(DIGAMMA_c)
-
-kernel void
-update_Elogtheta(long F, long K, const global float *gamma, global float *Elogtheta)
-
-				{
-				long d = get_global_id(0);
-
-				float acc = 0.0f;
-
-				for (long i=0; i<K; i++)
-					acc += gamma[K * (F + d) + i];
-
-				acc = digamma(acc);	
-
-				for (long i=0; i<K; i++)
-					Elogtheta[K * d + i] = digamma(gamma[K * (F + d) + i]) - acc;
-				}
-				"""
-
-function updateElogtheta!(model::gpuLDA, b::Int)
-	batch = model.batches[b]
-	model.queue(model.Elogthetakern, model.M, nothing, batch[1] - 1, model.K, model.gammabuf, model.Elogthetabuf)
+	model.queue(model.phi_norm_kernel, sum(model.N), nothing, model.K, model.phi_buffer)
 end
 
 function train!(model::gpuLDA; iter::Integer=150, tol::Real=1.0, niter::Integer=1000, ntol::Real=1/model.K^2, viter::Integer=10, vtol::Real=1/model.K^2, check_elbo::Real=1)
@@ -367,18 +382,17 @@ function train!(model::gpuLDA; iter::Integer=150, tol::Real=1.0, niter::Integer=
 			updateElogthetasum!(model, b)
 			updateNewBeta!(model)
 			if chk
-				updateHost!(model, b)
+				update_host!(model, b)
 				updateNewELBO!(model, b)
 			end
-			lowVRAM && updateBuf!(model, b)
 		end
-		updateAlpha!(model, niter, ntol)
-		updateBeta!(model)
-		if checkELBO!(model, k, chk, tol)
+		update_alpha!(model, niter, ntol)
+		update_beta!(model)
+		if check_elbo!(model, k, chk, tol)
 			break
 		end
 	end
-	updateHost!(model, 1)
+	update_host!(model, 1)
 	model.topics = [reverse(sortperm(vec(model.beta[i,:]))) for i in 1:model.K]
 	nothing
 end
