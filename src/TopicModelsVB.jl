2f660a69e4e6fe8a01cfec868d3421adb97fcf90
Eric Proffitt <eric.ai@icloud.com>
2020-05-31 13:18:07 -0400

    Update TopicModelsVB.jl

--- src/TopicModelsVB.jl
+++ src/TopicModelsVB.jl
@@ -11,7 +11,7 @@ using Crayons
 export Document, Corpus
 export TopicModel
 export LDA, CTM, fCTM, CTPF, gpuLDA, gpuCTM, gpuCTPF
-export readcorp, writecorp, abridge_corp!, alphabetize_corp!, compact_corp!, condense_corp!, pad_corp!, remove_empty_docs!, remove_redundant!, stop_corp!, trim_corp!, trim_docs!, fixcorp!, showdocs, showtitles, getlex, getusers
+export readcorp, writecorp, abridge_corp!, alphabetize_corp!, compact_corp!, condense_corp!, pad_corp!, remove_empty_docs!, remove_redundant!, stop_corp!, trim_corp!, trim_docs!, fixcorp!, showdocs, showtitles, getvocab, getusers
 export train!
 export @gpu
 export gendoc, gencorp, showtopics, showlibs, showdrecs, showurecs, predict, topicdist
