a9bcbc912cf82c4432689d42cb569da16bcfb2ab
Eric Proffitt <eric.ai@icloud.com>
2020-06-06 16:38:36 -0400

    up

--- src/LDA.jl
+++ src/LDA.jl
@@ -105,9 +105,9 @@ function update_alpha!(model::LDA, niter::Integer, ntol::Real)
 	nu = model.K
 	for _ in 1:niter
 		rho = 1.0
-		alpha_grad = (nu ./ model.alpha + model.M * (digamma(sum(model.alpha)) .- digamma.(model.alpha))) + Elogtheta_sum
+		alpha_grad = nu ./ model.alpha + model.M * (digamma(sum(model.alpha)) .- digamma.(model.alpha)) + Elogtheta_sum
 		alpha_invhess_diag = -1 ./ (model.M * trigamma.(model.alpha) + nu ./ model.alpha.^2)
-		p = (alpha_grad .- dot(alpha_grad, alpha_invhess_diag)  * (model.M * trigamma(sum(model.alpha)) + sum(alpha_invhess_diag))) .* alpha_invhess_diag
+		p = (alpha_grad .- dot(alpha_grad, alpha_invhess_diag) * (model.M * trigamma(sum(model.alpha)) + sum(alpha_invhess_diag))) .* alpha_invhess_diag
 		
 		while minimum(model.alpha - rho * p) < 0
 			rho *= 0.5
