bd4c92081e99758cac4aa99af1dcaa4690875960
Eric Proffitt <eric.ai@icloud.com>
2020-07-30 15:23:21 -0400

    Update utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -45,7 +45,7 @@ digamma(float x)
 		}
 		"""
 
-### Gauss-Jordan elimination (reduced row echelon form) algorithm for solving the linear system Ax=b.
+### Parallel Gauss-Jordan elimination (reduced row echelon form) algorithm for solving the linear system Ax=b.
 ### Partial pivoting unnecessary for symmetric positive-definite matrices.
 ### "Accuracy and Stability of Numerical Algorithms" Higham, 2002.
 const LINSOLVE_c =
